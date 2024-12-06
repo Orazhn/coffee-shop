3f9b07f22ec417a7cd037c6104d06fdfa9b32253
Orazkhn <orazhanovkydyrbek@gmail.com>
2024-12-06 18:53:11 +0500

    fix bugs 2

--- src/providers/Mode/ThemeButton.tsx
+++ src/providers/Mode/ThemeButton.tsx
@@ -7,18 +7,22 @@ import { useTheme } from "next-themes";
 import { Button } from "@/components/ui/button";
 
 export function ModeToggle() {
-  const { setTheme } = useTheme();
+  const { setTheme, theme } = useTheme();
 
   return (
     <Button variant="outline" size="icon">
-      <Sun
-        onClick={() => setTheme("dark")}
-        className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0 absolute"
-      />
-      <Moon
-        onClick={() => setTheme("light")}
-        className="h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100 absolute"
-      />
+      {theme == "dark" ? (
+        <Sun
+          onClick={() => setTheme("light")}
+          className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0 absolute"
+        />
+      ) : (
+        <Moon
+          onClick={() => setTheme("dark")}
+          className="h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100 absolute"
+        />
+      )}
+
       <span className="sr-only">Toggle theme</span>
     </Button>
   );
